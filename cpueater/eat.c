a2552589bab4c54c473cb82eb962a44d3e88ded9
Marius Vollmer <mvollmer@redhat.com>
2014-04-01 15:49:44 +0300

    Optionally reduce appetite of cpueater.

--- cpueater/eat.c
+++ cpueater/eat.c
@@ -1,6 +1,17 @@
 int
-main ()
+main (int argc, char **argv)
 {
+  int i;
+  int delay = 0;
+
+  if (argc > 1)
+    delay = atoi (argv[1]);
+
   while (1)
-    ;
+    {
+      for (i = 0; i < 10000000; i++)
+        ;
+      if (delay > 0)
+        usleep(delay);
+    }
 }
